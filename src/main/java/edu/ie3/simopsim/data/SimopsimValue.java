31ce003c8b7e2bebb18886b61541570fbf124bf0
Johannes Bao <smjobaoo@ie3.e-technik.tu-dortmund.de>
2024-10-10 15:08:29 +0200

    clean simosaik

--- src/main/java/edu/ie3/simopsim/data/SimopsimValue.java
+++
@@ -1,9 +0,0 @@
-package edu.ie3.simopsim.data;
-
-import de.fhg.iwes.opsim.datamodel.generated.realtimedata.OpSimMessage;
-import edu.ie3.simona.api.data.ExtInputDataValue;
-
-/**
- * Interface class that contains an OPSIM message
- */
-public record SimopsimValue(OpSimMessage opSimMessage) implements ExtInputDataValue {}
