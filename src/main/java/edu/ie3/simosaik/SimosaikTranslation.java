9b94e8704c23c1ea2cc8158e16b56cf5a9b4292f
Johannes Bao <smjobaoo@ie3.e-technik.tu-dortmund.de>
2024-10-10 15:04:18 +0200

    clean simosaik

--- src/main/java/edu/ie3/simosaik/SimosaikTranslation.java
+++
@@ -1,8 +0,0 @@
-package edu.ie3.simosaik;
-
-/** Conventions between SIMONA and MOSAIK */
-public class SimosaikTranslation {
-    public static final String MOSAIK_ACTIVE_POWER = "P[MW]";
-    public static final String MOSAIK_REACTIVE_POWER = "Q[MVAr]";
-    public static final String MOSAIK_VOLTAGE_DEVIATION = "deltaU[kV]";
-}
