a3e49068eb814f84a9e74eb5fea9c70dd5962ed1
staudtMarius <marius.staudt@tu-dortmund.de>
2024-11-20 07:27:46 +0100

    Saving changes.

--- src/main/java/edu/ie3/simosaik/ExtLink.java
+++ src/main/java/edu/ie3/simosaik/ExtLink.java
@@ -7,19 +7,20 @@
 package edu.ie3.simosaik;
 
 import edu.ie3.simona.api.ExtLinkInterface;
-import edu.ie3.simona.api.simulation.ExtSimulation;
+import edu.ie3.simona.api.simulation.ExtSimAdapterData;
 import edu.ie3.simosaik.simosaikElectrolyzer.MosaikElectrolyzerSimulation;
 
 public class ExtLink implements ExtLinkInterface {
-  private ExtSimulation extSim;
+  private MosaikElectrolyzerSimulation extSim;
 
   @Override
-  public ExtSimulation getExtSimulation() {
+  public MosaikElectrolyzerSimulation getExtSimulation() {
     return extSim;
   }
 
   @Override
-  public void setup(String[] mainArgs) {
-    extSim = new MosaikElectrolyzerSimulation(mainArgs);
+  public void setup(ExtSimAdapterData data) {
+    extSim = new MosaikElectrolyzerSimulation(data.getMainArgs());
+    extSim.setAdapterData(data);
   }
 }
